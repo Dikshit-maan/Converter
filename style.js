edb102b9c22232926be2a2862d12c1b1a368465a
Dikshit-maan <149257007+Dikshit-maan@users.noreply.github.com>
2024-01-23 07:22:59 +0000

    on..

--- style.js
+++ style.js
@@ -2,12 +2,33 @@ const BASE_URL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/
 
 const dropdowns = document.querySelectorAll(".dropdown select");
 
-
-for(let select of dropdowns){
-    for ( currCode in countryList){
-        let newOption = document.createElement("option");
-        newOption.innerText = currCode;
-        newOption.value = currOption;
-        select.append(newOption);
-    }
+for (code in CountryList){
+    console.log(code);
 }
+
+
+
+// for(let select of dropdowns){
+//     for ( currCode in countryList){
+//         let newOption = document.createElement("option");
+//         newOption.innerText = currCode;
+//         newOption.value = currOption;
+//         select.append("newOption");
+//     }
+// }
+
+
+// const BASE_URL =
+// "https://cdn.jsdelivr.net/gh/fawazahmedo/currency-api@1/latest/currencies/usd/inr.json"
+// const dropdowns = document.querySelectorAll(".dropdown select");
+// for (let select of dropdowns) {
+// for (currCode in countryList) {
+// let newOption = document.createElement("option");
+// newOption.innerText = currCode;
+// newOption.value = currCode;
+// if (select.name === "from" && currCode === "USD") {
+// newOption.selected = "selected";
+// }
+// select.append(newOption);
+// }
+// }
